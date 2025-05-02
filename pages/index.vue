0694c14ada1ff4cd8e274100829f12563c4545f0
Chandra Rohith <98586047+chandrarohith3@users.noreply.github.com>
2025-05-02 12:29:55 +0530

    Update Image path

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
             <div class="text-center">
               <!-- Image -->
                <div class="mb-8">
-                <img src="https://media.licdn.com/dms/image/v2/D5603AQFsa7yfHfUYNg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1710818668635?e=1744848000&v=beta&t=xbT4Ur7P4NyfJj2U79opYLni8X4-xpWRTb9D-OumJIU" alt="Chandra Rohith" class="rounded-full w-48 h-48 mx-auto object-cover" />
+                <img src="https://media.licdn.com/dms/image/v2/D5603AQFsa7yfHfUYNg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1710818668635?e=1751500800&v=beta&t=mMkXMsNKxS600PUv0wB6Gi9iRO6Rfj2gb_SYU3HAbOo" alt="Chandra Rohith" class="rounded-full w-48 h-48 mx-auto object-cover" />
               </div>
               <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
                 <span class="block">Hi, I'm</span>
