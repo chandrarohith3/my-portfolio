8c0fd76cb34d8e72d33a170051094226527bf2f3
chandrarohith3 <chandrarohith3@gmail.com>
2025-05-25 20:28:26 +0530

    Updated the pic

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,11 @@
             <div class="text-center">
               <!-- Image -->
                <div class="mb-8">
+<<<<<<< HEAD
                 <img src="https://media.licdn.com/dms/image/v2/D5603AQFsa7yfHfUYNg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1710818668635?e=1751500800&v=beta&t=mMkXMsNKxS600PUv0wB6Gi9iRO6Rfj2gb_SYU3HAbOo" alt="Chandra Rohith" class="rounded-full w-48 h-48 mx-auto object-cover" />
+=======
+                <img src="C:\Users\chand\Downloads\project-c\1710818668635.jpeg" alt="Chandra Rohith" class="rounded-full w-48 h-48 mx-auto object-cover" />
+>>>>>>> 7beb38d (Updated the pic)
               </div>
               <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
                 <span class="block">Hi, I'm</span>
